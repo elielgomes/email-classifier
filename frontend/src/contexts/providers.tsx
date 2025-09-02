4c73ac384c36d6b022ab668f83123e338b43c295
Eliel Gomes <elielgomespg@hotmail.com>
2025-09-02 15:00:00 -0300

    :sparkles: feat: Add theme provider

---
+++ frontend/src/contexts/providers.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import { Toaster } from "@/components/ui/sonner";
+import { ThemeProvider } from "./themes.provider";
+
+export const Providers = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <ThemeProvider
+      attribute="class"
+      defaultTheme="system"
+      enableSystem
+      disableTransitionOnChange
+    >
+      {children}
+      <Toaster richColors />
+    </ThemeProvider>
+  );
+};
